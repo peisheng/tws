c48964f999262862da18096e575a2adb110f726d
peisheng <279805791@qq.com>
2015-12-22 17:59:44 +0800

    add

--- admin/src/js/controllers/todaystart/operates.js
+++ admin/src/js/controllers/todaystart/operates.js
@@ -1,1 +1,39 @@
+app.controller('OperatesCtrl', ['$scope', "$http", function($scope, $http) {
+    var page_size = 15;
+    var getDataList = function() {
+        var params = {
+            page_index: $scope.paginationConf.currentPage,
+            page_size: $scope.paginationConf.itemsPerPage
+        }
 
+        var q = $http({
+            method: "GET",
+            url: _Api + "/admin/operate/list",
+            params: params
+        });
+
+        q.success(function(data) {
+            $scope.paginationConf.totalItems = data.total_count;
+            $scope.items = data.items;
+
+        });
+    }
+
+    $scope.ckSearch = function() {
+        $scope.paginationConf.currentPage = 1;
+        getDataList();
+    }
+
+    $scope.paginationConf = {
+        currentPage: 1,
+        totalItems: 0,
+        itemsPerPage: page_size,
+        pagesLength: 10,
+        perPageOptions: [10, 20, 50],
+        onChange: function(e) {}
+    };
+
+    $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', function() {
+        getDataList();
+    });
+}]);
